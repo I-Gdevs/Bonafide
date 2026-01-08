034071e610733d5fea5d5ceeb21f8f74bbf37116
Ignacio Imas <ignacioimas17@gmail.com>
2026-01-07 21:06:40 -0300

    Fixed navbar company name to test auto-deployment

--- views/partials/header.php
+++ views/partials/header.php
@@ -7,7 +7,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="<?= BASE_URL ?>/home">CAFEJTERIA ROJA</a>
+            <a class="navbar-brand" href="<?= BASE_URL ?>/home">BONAFIDE</a>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
