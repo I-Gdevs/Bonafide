56371f6753e964f17541ac87c87181c4076b1d69
Ignacio Imas <ignacioimas17@gmail.com>
2026-01-07 23:10:39 -0300

    Fixed define BASE_URL on index file

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
         $protocol = 'https';
     } elseif (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-        $protocol = 'http';
+        $protocol = 'https';
     } else {
         $protocol = 'http';
     }
